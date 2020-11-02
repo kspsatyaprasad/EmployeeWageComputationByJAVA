5c7f01b47bfb8765d836acca002bba636967c312
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-02 20:40:38 +0530

    added code for daily employee wage calculation

--- employeewage_computation_by_java.java
+++ employeewage_computation_by_java.java
@@ -1,17 +1,19 @@
-public class EmployeeWageComputation_UC_1
+public class EmployeeWageComputation_UC_2
 {
     public static void main(String args[])
     {
-        
-        int employee_Attendance = (int) (Math.random() * 100) % 2;
-        if (employee_Attendance == 1)
+        final int WAGE_PER_HOUR = 20;
+        int type_of_Employee = (int) (Math.random() * 100) % 2;
+        int hours_Worked = 0;
+        if (type_of_Employee == 1)
         {
             System.out.println("Employee is Present");
-
+            hours_Worked = 8;
         } else
         {
             System.out.println("Employee is Absent");
-
         }
+        int wage = hours_Worked * WAGE_PER_HOUR;
+        System.out.println("Daily Wage of Employee is " + wage);
     }
 }
