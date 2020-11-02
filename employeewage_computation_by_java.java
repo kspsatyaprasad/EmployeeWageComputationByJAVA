72b34868628848ed35e121bd512cac44d9516602
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-02 20:47:26 +0530

    added code for monthly wages calculation

--- employeewage_computation_by_java.java
+++ employeewage_computation_by_java.java
@@ -1,26 +1,31 @@
-public class EmployeeWageComputation_UC_4
+public class EmployeeWageComputation_UC_5
 {
     public static void main(String args[])
     {
         final int PART_TIME = 1;
         final int FULL_TIME = 2;
         final int WAGE_PER_HOUR = 20;
-        int type_of_Employee = (int) (Math.random() * 100) % 3;
-        int hours_Worked =0;
-        switch (type_of_Employee)
+        final int WORKING_DAYS = 20;
+    
+        int total_earned_Wage = 0;
+        for (int day = 1; day <= WORKING_DAYS; day++)
         {
-            case FULL_TIME:
-                System.out.println("Employee is Present and he worked  Full time");
-                hours_Worked = 8;
-                break;
-            case PART_TIME:
-                System.out.println("Employee is Present and he worked Part time");
-                hours_Worked = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent");    
+            int type_of_Employee = (int) (Math.random() * 100) % 3;
+            int hours_Worked =0;
+            switch (type_of_Employee)
+            {
+                case FULL_TIME:
+                    hours_Worked = 8;
+                    break;
+                case PART_TIME:
+                    hours_Worked = 4;
+                    break;
+                default:
+            }
+            int wage = hours_Worked * WAGE_PER_HOUR;
+            System.out.println(" Wage earned in Day" + day + " is:" + wage);
+            total_earned_Wage += wage;
         }
-        int wage = hours_Worked * WAGE_PER_HOUR;
-        System.out.println("Employee Daily Wage is " + wage);
+        System.out.println("Total wage earned in a month is " + total_earned_Wage);        
     }
 }
