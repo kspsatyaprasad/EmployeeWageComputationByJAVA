27e72e9f8d57a626e3f5c505710d07d4fa981401
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-02 20:42:18 +0530

    added code for calculating parttime employee wage

--- employeewage_computation_by_java.java
+++ employeewage_computation_by_java.java
@@ -1,14 +1,20 @@
-public class EmployeeWageComputation_UC_2
+public class EmployeeWageComputation_UC_3
 {
     public static void main(String args[])
     {
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
         final int WAGE_PER_HOUR = 20;
-        int type_of_Employee = (int) (Math.random() * 100) % 2;
+        int type_of_Employee = (int) (Math.random() * 100) % 3;
         int hours_Worked = 0;
-        if (type_of_Employee == 1)
+        if (type_of_Employee == FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present Full time");
             hours_Worked = 8;
+        } else if (type_of_Employee == PART_TIME)
+        {
+            System.out.println("Employee is Present Part time");
+            hours_Worked = 4;
         } else
         {
             System.out.println("Employee is Absent");
