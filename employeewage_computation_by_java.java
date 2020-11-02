6ed00f7c742ce04d89f7e5c0a61a637c3e1db73d
kspsatyaprasad <koppulasatyaprasad@gmail.com>
2020-11-02 20:38:54 +0530

    added code for checking employee attendance

--- employeewage_computation_by_java.java
+++ employeewage_computation_by_java.java
@@ -1,9 +1,17 @@
-public class EmployeeWageComputation
+public class EmployeeWageComputation_UC_1
 {
     public static void main(String args[])
     {
         
-     System.out.println("Welcome to Employee Wage Computation Problem Using JAVA");
+        int employee_Attendance = (int) (Math.random() * 100) % 2;
+        if (employee_Attendance == 1)
+        {
+            System.out.println("Employee is Present");
 
+        } else
+        {
+            System.out.println("Employee is Absent");
+
+        }
     }
 }
